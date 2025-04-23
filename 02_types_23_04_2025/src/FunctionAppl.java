c9fadb872aa4adcc8c7951797b53a48873aa976e
Evgeniy Margovich <evmargovich@gmail.com>
2025-04-23 16:14:51 +0300

    Test init. Second lesson

---
+++ 02_types_23_04_2025/src/FunctionAppl.java
@@ -0,0 +1,15 @@
+public class FunctionAppl {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        improgrammer();
+    }
+
+    public static void improgrammer() {
+        System.out.println("I am a programmer!");
+        author();
+    }
+
+    private static void author() {
+        System.out.println("Jack");
+    }
+}
---
+++ 02_types_23_04_2025/src/TypesAppl.java
@@ -0,0 +1,24 @@
+public class TypesAppl {
+    public static void main(String[] args) {
+
+        byte bits = 20;
+        short a = 1_300;
+        int c = 2_000_000_000;
+        long l = 20_000_000_000L;
+        float f = 2.9f;
+        double d = 1298989898989.2;
+        char ch = 103;
+        boolean b = true;
+
+        System.out.println("byte: " + bits);
+        System.out.println("short: " + a);
+        System.out.println("int: " + c);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + ch);
+        System.out.println("boolean: " + b);
+
+    }
+
+}
