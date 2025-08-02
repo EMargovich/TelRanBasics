ed0b2529746b9b471435e029ca671b81b7e47cd4
Evgeniy Margovich <evmargovich@gmail.com>
2025-08-02 15:44:52 +0300

    Homework_27

--- hw_27_Fibonacci/src/telran/fibonacci/model/FibonacciIterator.java
+++ hw_27_Fibonacci/src/telran/fibonacci/model/FibonacciIterator.java
@@ -4,13 +4,13 @@ import java.util.Iterator;
 
 public class FibonacciIterator implements Iterator<Integer> {
 
-    private int index = 0;
+    private int index = 1;
     private int quantity;
     private int num1 = 0;
     private int num2 = 1;
 
     public FibonacciIterator(int quantity) {
-        this.quantity = quantity-1;
+        this.quantity = quantity;
     }
 
     @Override
